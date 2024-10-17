4b468b94655b53e2975668dd65168f9936880c97
MatthewMartin93 <161871982+MatthewMartin93@users.noreply.github.com>
2024-10-17 08:23:31 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,21 +1,27 @@
 #include <iostream>
-#include <stdio.h>
 using namespace std;
 int main()
 {
-    printf("What is your name?");
+    printf("What is your name? ");
     char name[100];
     scanf("%s", name);
-    printf("How old are you?");
+
+    printf("How old are you? ");
     int age;
     scanf("%d", &age);
-    printf("What the initial of your last name?");
+
+    printf("What the initial of your last name? ");
     char initial;
     scanf("%s", &initial);
-    printf("What is your grade average?");
+
+    printf("What is your grade average? ");
     float avg;
     scanf("%f", &avg);
-    printf("%s\n%d\n%s\n%.2f", name, age, initial, avg);
+
+    printf("Name: %s\n", &name);
+    printf("Age: %d\n", &age);
+    printf("Initial: %s\n", &initial);
+    printf("Grade Average: %s\n", &avg);
 
 
 }
